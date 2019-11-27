c59c708301c79e86953cba432808f446550ce0a8
9640_gdev (Student) <s_9640_gdev@PEDAGOGIQUE.LAN>
2019-11-27 15:30:08 +0100

    added acceleration behavior

--- Assets/Scripts/CameraBehavior.cs
+++ Assets/Scripts/CameraBehavior.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class CameraBehavior : MonoBehaviour
 {
 	Player playerToFollow;
+	public bool sliding;
     // Start is called before the first frame update
     void Start()
     {
-        
+		sliding = false;
     }
 
     // Update is called once per frame
@@ -18,6 +19,18 @@ public class CameraBehavior : MonoBehaviour
 		{
 			playerToFollow = FindObjectOfType<Player>();
 		}
-		gameObject.transform.position = new Vector3(playerToFollow.gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+		SlidingCamera();
     }
+
+	public void SlidingCamera()
+	{
+		if (sliding)
+		{
+			gameObject.transform.position = new Vector3(playerToFollow.gameObject.transform.position.x + 0.5f, gameObject.transform.position.y, gameObject.transform.position.z);
+		} else
+		{
+			gameObject.transform.position = new Vector3(playerToFollow.gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+		}
+		//gameObject.transform.Translate(0.5f * horizontal, 0, 0);
+	}
 }
